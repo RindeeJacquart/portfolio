1bebce4e1e945b7556f13bc6e91317659ac63e48
Kelly Cook <kellyrcook1980@gmail.com>
2023-03-08 22:37:38 -0700

    nav bar mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useLocation } from 'react-router-dom';
 
-import { useIsMobile } from '../hooks/useIsMobile'
+import { useIsMobile } from '../hooks/useIsMobile';
 import logo from '../assets/logo.png';
+import menuIcon from '../assets/menuIcon.svg';
 import './nav.css';
 
+import MobileMenu from './MobileMenu';
+
 export default function NavBar() {
+  const location = useLocation();
   const isMobile = useIsMobile();
+  const [showMenu, setShowMenu] = useState(false);
+  const [pathname] = useState(location.pathname)
 
   return (
     <div className='navBar'>
-      <img src={logo} alt='brand logo' className='navLogo' />
+      <div className='navLogoWrapper'>
+       <a href='/'> <img src={logo} alt='brand logo' className='navLogo' /></a>
+      </div>
       <div className='brandContainer'>
         <h2 className='brandName'>Rindee Jacquart</h2>
         <p className='brandText'>Digital Jaguar</p>
         <p className='brandText'>Media</p>
       </div>
-      { isMobile ? 
-        <ul className='navLinksMobile'>
-        <li><a className='linkText'>Websites</a></li>
-        <li><a className='linkText'>Business</a></li>
-        <li><a className='linkText'>Other Designs</a></li>
-      </ul>
-      : 
-      <ul className='navLinks'>
-        <li><a className='linkText'>Websites</a></li>
-        <li><a className='linkText'>Business</a></li>
-        <li><a className='linkText'>Other Designs</a></li>
-      </ul>
-}
+
+      <div className='navLinks'>
+        {!isMobile ? (
+          <ul className='navLinksWrapper'>
+            <li>
+              <a href='/Websites' className={pathname === '/Websites' ? 'linkTextActive' : 'linkText'}>Websites</a>
+            </li>
+            <li>
+              <a href='/business' className={pathname === '/business' ? 'linkTextActive' : 'linkText'}>Business</a>
+            </li>
+            <li>
+              <a 
+             href='/other-designs' className={pathname === '/other-designs' ? 'linkTextActive' : 'linkText'}>Other Designs</a>
+            </li>
+          </ul>
+        ) : (
+          <div className='menuWrapper'>
+          <div className='menuIcon'>
+            <img src={menuIcon} alt='nav menu' className='icon' onClick={() => setShowMenu(!showMenu)}/>
+            {showMenu ? <MobileMenu pathname={pathname} /> : null}
+          </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
