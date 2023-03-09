7ee41b9e9c04ae0619acd6513789ed440021a122
Kelly Cook <kellyrcook1980@gmail.com>
2023-03-08 22:37:18 -0700

    update app for routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,1 @@
-import Home from './pages/home/Home'
-import './App.css';
-import NavBar from './components/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-     <Home />
-    </div>
-  );
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout/Layout';
+import Home from './pages/home/Home';
+import Business from './pages/business/Business';
+import './App.css';
+import Websites from "./pages/Websites/Websites";
+import OtherDesigns from "./pages/other-designs/OtherDesigns";
 
-render(<App />, document.getElementById('root'));
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+        <Route path='/business' element={<Business />} />
+        <Route path='/websites' element={<Websites />} />
+        <Route path='/other-designs' element={ <OtherDesigns/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
