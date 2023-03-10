48ad0a24f015954ba0bfeb80505618de25f5bc3b
Kelly Cook <kellyrcook1980@gmail.com>
2023-03-10 01:28:20 -0700

    format

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Layout from './pages/Layout/Layout';
 import Home from './pages/home/Home';
 import Business from './pages/business/Business';
 import './App.css';
-import Websites from "./pages/Websites/Websites";
-import OtherDesigns from "./pages/other-designs/OtherDesigns";
+import Websites from './pages/Websites/Websites';
+import OtherDesigns from './pages/other-designs/OtherDesigns';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-        <Route path='/business' element={<Business />} />
-        <Route path='/websites' element={<Websites />} />
-        <Route path='/other-designs' element={ <OtherDesigns/>} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/business' element={<Business />} />
+          <Route path='/websites' element={<Websites />} />
+          <Route path='/other-designs' element={<OtherDesigns />} />
         </Route>
       </Routes>
     </BrowserRouter>
