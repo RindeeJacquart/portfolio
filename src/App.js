e5065eeceef471999568896b9d1b80495ea2f202
Kelly Cook <kellyrcook1980@gmail.com>
2023-03-10 01:16:17 -0700

    app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout/Layout';
+import Home from './pages/home/Home';
+import Business from './pages/business/Business';
+import './App.css';
+import Websites from "./pages/Websites/Websites";
+import OtherDesigns from "./pages/other-designs/OtherDesigns";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+        <Route path='/business' element={<Business />} />
+        <Route path='/websites' element={<Websites />} />
+        <Route path='/other-designs' element={ <OtherDesigns/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
